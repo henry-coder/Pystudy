9b42370cec83e18e0572f5c24f33bf2496008bcb
henry-coder <henry-lemos@hotmail.com.br>
2020-04-06 20:09:24 -0300

    Criação do ex002
    faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas

---
+++ ex002 resposta ao usuario.py
@@ -0,0 +1,6 @@
+"""
+EXERCÍCIO 002: Respondendo ao Usuário
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+"""
+nome = input('Digite seu nome: ')
+print('Prazer em conhece-lo,', nome)
