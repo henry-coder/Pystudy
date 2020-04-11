548c150091c7578bcec88f01cd270547312fd441
henry-coder <henry-lemos@hotmail.com.br>
2020-04-11 20:35:57 -0300

    Criação do ex005
    Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.

---
+++ ex005 antecessor e sucessor.py
@@ -0,0 +1,6 @@
+"""
+EXERCÍCIO 005: Antecessor e Sucessor
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+"""
+n = int(input('Digite um número inteiro: '))
+print('o antecessor de {} é {} e o sucessor é {}'.format(n,n-1,n+1))
