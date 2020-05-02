64df037ce63a9f6e3df6059565c380c6a15eb7be
henry-coder <henry-lemos@hotmail.com.br>
2020-05-01 23:49:46 -0300

    Criação do ex007
    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.

---
+++ ex007 média aritmetica.py
@@ -0,0 +1,7 @@
+"""
+EXERCÍCIO 007: Média Aritmética
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+"""
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print('A média entre {} e {} é: {}'.format(n1,n2,(n1+n2)/2))
